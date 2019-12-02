1d84e029dfcf8f059fb5437961f1d62b94b86aa1
Cathy <cathy455503@gmail.com>
2019-12-01 21:40:43 -0500

    walkhistory update on load

--- public/js/walkHistory.js
+++ public/js/walkHistory.js
@@ -3,6 +3,35 @@
 // Initialize the page
 window.addEventListener("load", initializePage);
 
+function helperforWalker(url) {
+  const name = [];
+  fetch(url).then((res) => {
+    if (res.status === 200) {
+        return res.json();
+    }
+    else {
+        console.log("Error " + res.status + ": Could not get user data");
+        return Promise.reject(res.status);
+    }
+  }).then((walker) => {
+    const fname = walker.firstName;
+    const lname = walker.lastName;
+    name.push(fname, lname);
+    console.log(name)
+    return name;
+
+    // walkerName.innerText = fname + " " lname;
+  }).catch((error) => {
+    if (error === 404) {
+        alert("Session expired! Please log in again");
+        window.location.href = "login.html";
+    }
+    else {
+        console.log("????");
+    }
+  });
+}
+
 function initializePage(e) {
   // first find the user
   const url = '/user';
@@ -28,12 +57,9 @@ function initializePage(e) {
         return Promise.reject(res.status);
       }
     }).then((walks) => {
-      // got all walks for user, update the dom here
-      // TODO need to get completed walks in data
-      console.log(walks)
-
+      // got all walks for user, update the view
       $.each(walks, function (index, value) {
-        console.log(value);
+        const name = [];
 
         const doc = document.getElementById("main");
         const container = document.createElement("div");
@@ -45,9 +71,39 @@ function initializePage(e) {
         walkHistory.id = "walkHistory";
         container.appendChild(walkHistory);
 
+        // get walker name with id
+        const walkUrl = '/walker/' + String(value.walkerId);
+        // const name = setTimeout(helperforWalker(walkUrl), 500);
+
+        fetch(walkUrl).then((res) => {
+          if (res.status === 200) {
+              return res.json();
+          }
+          else {
+              console.log("Error " + res.status + ": Could not get user data");
+              return Promise.reject(res.status);
+          }
+        }).then((walker) => {
+          const fname = walker.firstName;
+          const lname = walker.lastName;
+          name.push(fname, lname);
+          return name;
+          // walkerName.innerText = fname + " " lname;
+        }).catch((error) => {
+          if (error === 404) {
+              alert("Session expired! Please log in again");
+              window.location.href = "login.html";
+          }
+          else {
+              console.log("????");
+          }
+        });
+
+        console.log(name)
+
         const walkerName = document.createElement("p");
-        // add the name later
         walkerName.id = "walkerName";
+        walkerName.innerText = name[0] + " " + name[1];
 
         const walkDate = document.createElement("span");
         walkDate.id = "walkDate";
@@ -73,7 +129,7 @@ function initializePage(e) {
         ratingtxt.innerText = "Walk Rating";
         rating.appendChild(ratingtxt);
         const ratingtxt2 = document.createElement("p");
-        // have rating update HERE
+        ratingtxt2.innerText = value.walkerRating;
         rating.appendChild(ratingtxt2);
         row.appendChild(rating);
 
@@ -84,7 +140,7 @@ function initializePage(e) {
         durationtxt.innerText = "Duration"
         duration.appendChild(durationtxt);
         const durationtxt2 = document.createElement("p");
-        //add duration HERE
+        durationtxt2.innerText = value.duration;
         duration.appendChild(durationtxt2);
         row.appendChild(duration);
 
@@ -95,40 +151,12 @@ function initializePage(e) {
         pricetxt.innerText = "Walk Price"
         price.appendChild(pricetxt);
         const pricetxt2 = document.createElement("p");
-        //add price HERE
+        pricetxt2.innerText = value.price;
         price.appendChild(pricetxt2);
         row.appendChild(price);
 
         walkHistory.appendChild(row);
       });
-
-      // get walker name with id
-      const walkerID = json.walkerId
-      const walkerUrl = '/walker/walkerID'
-      fetch(url).then((res) => {
-        if (res.status === 200) {
-            return res.json();
-        }
-        else {
-            console.log("Error " + res.status + ": Could not get user data");
-            return Promise.reject(res.status);
-        }
-      }).then((walker) => {
-        const fname = walker.firstName;
-        const lname = walker.lastName;
-        //update dom
-        console.log(walker)
-
-
-      }).catch((error) => {
-        if (error === 404) {
-            alert("Session expired! Please log in again");
-            window.location.href = "login.html";
-        }
-        else {
-            console.log("????");
-        }
-      })
       }).catch((error) => {
         if (error === 404) {
             alert("Session expired! Please log in again");
