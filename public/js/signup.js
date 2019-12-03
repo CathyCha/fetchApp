20f8b4261d1ed3eee29efc53e37544ce14db61a6
Dhruvin Vekariya <dhruvin@Dhruvins-MacBook-Pro.local>
2019-12-03 11:23:54 -0500

    signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,58 +3,62 @@ const log = console.log
 // log('Loaded front-end javascript.')\
 // const bcrypt = require('bcryptjs')
 function addUser(){
+    var usertype;
+    if(document.getElementById("radio-one").checked){
+        usertype = "/user"
+    }else if(document.getElementById("radio-one").checked){
+        usertype = "/walker"
+    }
 
-	const url = "http://localhost:3001/user";
-	let data = {
-		firstName: document.querySelector('#firstName').value,
+
+    // const url = "http://localhost:3001/user";
+    let data = {
+        firstName: document.querySelector('#firstName').value,
         lastName: document.querySelector('#lastName').value,
         password: document.querySelector('#password').value,
-        username: document.querySelector('#userName').value,
-        homeAddress: document.querySelector('#address').value,
+        username: document.querySelector('#username').value,
+        homeAddress: document.querySelector('#homeAddress').value,
         city: document.querySelector('#city').value,
         province: document.querySelector('#province').value,
         phoneNumber: document.querySelector('#phoneNumber').value,
         emailAddress: document.querySelector('#emailAddress').value
     }
  
-	const request = new Request(url, {
+    const request = new Request(usertype, {
         method: 'post', 
         body: JSON.stringify(data),
         headers: {
             'Accept': 'application/json, text/plain, */*',
             'Content-Type': 'application/json'
         },
-        mode: 'cors'
-    })
+        mode: 'cors',
+        credentials: 'include'
+    });
 
     fetch(request)
-    .then(function(res) {
+    .then((res) => {
+        log("lsoldolsodlsdosldosdlsdosldsodlsodsldosdlsodsldosdlsodsldosd")
 
-    	// TODO: Nottify users that they their account is created
-
-        // Handle response we get from the API.
-        // Usually check the error codes to see what happened.
-        // const message = document.querySelector('#message')
-        if (res.status === 200) {
-            // If student was added successfully, tell the user.
-            // console.log('Added student')
-            // message.innerText = 'Success: Added a student.'
-            // message.setAttribute("style", "color: green")
-            alert("IT works !!")
-            console.log("dsds")
-           
-        } else {
-        //     // If server couldn't add the student, tell the user.
-        //     // Here we are adding a generic message, but you could be more specific in your app.
-        //     message.innerText = 'Could not add student'
-        //     message.setAttribute("style", "color: red")
-        console.log("Something went wrong")
-     
+        let newData = {
+        password: document.querySelector('#password').value,
+        username: document.querySelector('#username').value,
+        userType: usertype,
         }
-        // log(res)  // log the result in the console for development purposes,
-                          //  users are not expected to see this.
-    }).catch((error) => {
-        console.log(error)
+        log(newData)
+        const url = "/login"
+        request = new Request(url, {
+        method: 'post', 
+        body: JSON.stringify(data),
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        mode: 'cors',
+        credentials: "include"
+        });
+        return fetch(request)
+    }).catch((error)=>{
+        log(error)
     })
-
+        
 }
