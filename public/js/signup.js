f5b1564cde8dde2528ecc2616451467bc0355e04
Dhruvin Vekariya <39427569+DhruvinV@users.noreply.github.com>
2019-12-03 15:32:12 -0500

    Update signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,15 +3,15 @@ const log = console.log
 // log('Loaded front-end javascript.')\
 // const bcrypt = require('bcryptjs')
 function addUser(){
-    var usertype;
-    if(document.getElementById("radio-one").checked){
-        usertype = "/user"
-    }else if(document.getElementById("radio-one").checked){
-        usertype = "/walker"
-    }
-
-
-    // const url = "http://localhost:3001/user";
+    // var usertype;
+    // if(document.getElementById("radio-one").checked){
+    //     usertype = "/user"
+    // }else if(document.getElementById("radio-one").checked){
+    //     usertype = "/walker"
+    // }
+    // console.log(document.getElementById("signupForm").elements["userswitch"].value)
+    const userType = "/"+document.getElementById("signupForm").elements["userswitch"].value
+    console.log(userType)
     let data = {
         firstName: document.querySelector('#firstName').value,
         lastName: document.querySelector('#lastName').value,
@@ -23,42 +23,25 @@ function addUser(){
         phoneNumber: document.querySelector('#phoneNumber').value,
         emailAddress: document.querySelector('#emailAddress').value
     }
- 
-    const request = new Request(usertype, {
-        method: 'post', 
+    // console.log(userType)
+    const request = new Request(userType, {
+        method: 'POST', 
         body: JSON.stringify(data),
         headers: {
             'Accept': 'application/json, text/plain, */*',
             'Content-Type': 'application/json'
         },
-        mode: 'cors',
-        credentials: 'include'
+        mode: 'cors'
     });
-
     fetch(request)
-    .then((res) => {
-        log("lsoldolsodlsdosldosdlsdosldsodlsodsldosdlsodsldosdlsodsldosd")
-
-        let newData = {
-        password: document.querySelector('#password').value,
-        username: document.querySelector('#username').value,
-        userType: usertype,
+    .then(function(res){
+        if(res.status === 200){
+            location.replace("/login.html")
+            alert("Account Created, Pleaser login in")
+        }else{
+            alert('Could not create account, Please try again')
         }
-        log(newData)
-        const url = "/login"
-        request = new Request(url, {
-        method: 'post', 
-        body: JSON.stringify(data),
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        },
-        mode: 'cors',
-        credentials: "include"
-        });
-        return fetch(request)
     }).catch((error)=>{
         log(error)
-    })
-        
+    })      
 }
